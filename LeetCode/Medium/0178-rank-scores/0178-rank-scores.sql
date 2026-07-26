820a3253789cf743403585c493e587007e8e9dea
Shivendra Thakur <102912377+shiv-0101@users.noreply.github.com>
2026-07-26 23:38:58 +0530

    Time: 400 ms (22.56%), Space: 0B (100%) - CodeTrack

---
+++ LeetCode/Medium/0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    S.score,
+    COUNT(S2.score) AS `rank`
+FROM Scores S,
+     (SELECT DISTINCT score FROM Scores) S2
+WHERE S.score <= S2.score
+GROUP BY S.id
+ORDER BY S.score DESC;
